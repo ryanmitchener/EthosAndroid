2314cb8ac7e464a56edb586850b1f4403131cf2c
Ryan Mitchener <RyanMitchener@gmail.com>
2016-10-02 01:32:17 -0400

    - Added UI Test module which will be used for views

---
+++ uitest/src/main/java/com/ethossoftworks/uitest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ethossoftworks.uitest;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ethossoftworks.ethos.Promise;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
